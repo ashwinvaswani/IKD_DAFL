8b1e98dd45e581e06fe24af998c207de209a0571
Het-Shah <divhet163@gmail.com>
2020-03-19 22:22:45 +0530

    Add student_train and modify utils

--- code/utils.py
+++ code/utils.py
@@ -24,12 +24,12 @@ def adjust_learning_rate(optimizer, epoch, learing_rate):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
         
-def train_model(net, teacher, generator, data_test_loader, device, criterion, optimizer_G, optimizer_S, lr_G=0.2, lr_S= 2e-3, one_hot=1, information_entropy=10, activation=0.1, batch_size=32, img_size=32, latent_dim=100, n_epochs=200, dataset='MNIST'):
+def train_model(net, teacher, generator, data_test_loader, device, criterion, optimizer_G, optimizer_S, lr_G=0.2, lr_S= 2e-3, one_hot=1, information_entropy=10, activation=0.1, batch_size=32, img_size=32, latent_dim=100, n_epochs=200, dataset='MNIST',cnt=0):
     accr = 0
     accr_best = 0
     batches_done = 0
+    teacher.eval()
     for epoch in range(n_epochs):
-
         total_correct = 0
         avg_loss = 0.0
         if dataset != 'MNIST':
@@ -56,20 +56,23 @@ def train_model(net, teacher, generator, data_test_loader, device, criterion, op
             optimizer_S.step() 
             if i == 1:
                 print ("[Epoch %d/%d] [loss_oh: %f] [loss_ie: %f] [loss_a: %f] [loss_kd: %f]" % (epoch, n_epochs,loss_one_hot.item(), loss_information_entropy.item(), loss_activation.item(), loss_kd.item()))
-
         with torch.no_grad():
             for i, (images, labels) in enumerate(data_test_loader):
-                images = images.cuda()
-                labels = labels.cuda()
+                images = images.to(device)
+                labels = labels.to(device)
                 net.eval()
                 output = net(images)
                 avg_loss += criterion(output, labels).sum()
                 pred = output.data.max(1)[1]
-                total_correct += pred.eq(labels.data.view_as(pred)).sum()
+                total_correct += torch.sum(pred == labels.data)
 
         avg_loss = avg_loss / (len(data_test_loader)*batch_size)
-        print('Test Avg. Loss: %f, Accuracy: %f' % (avg_loss.data.item(), float(total_correct) / (len(data_test_loader)*batch_size)))
         accr = round(float(total_correct) / (len(data_test_loader)*batch_size), 4)
+        print('Test Avg. Loss: %f, Accuracy: %f' % (avg_loss.data.item(), accr))
+        
         if accr > accr_best:
-            torch.save(net,'student')
             accr_best = accr
+            print("Saving model with accuracy: {}".format(accr_best))
+            cnt = cnt+1
+            torch.save(net.state_dict(),'student_'+str(cnt)+'_'+str(dataset)+'.pt')
+    return accr_best, cnt    
